be0ec93e4a47862327228c1bb327f10cce9773fe
Mykola Slepko <mslep@softserveinc.com>
2020-09-24 11:44:42 +0300

    Updates

---
+++ precache-manifest.12b33422e94922f2cfc9d35631218982.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f963ee6b47c552c750bedb43f413c3",
+    "url": "/Country-List/index.html"
+  },
+  {
+    "revision": "f707c673db47389fce90",
+    "url": "/Country-List/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a8ca5a7bb1deae3da0c1",
+    "url": "/Country-List/static/css/main.344e25a1.chunk.css"
+  },
+  {
+    "revision": "f707c673db47389fce90",
+    "url": "/Country-List/static/js/2.6c8aac17.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/Country-List/static/js/2.6c8aac17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ca5a7bb1deae3da0c1",
+    "url": "/Country-List/static/js/main.47a37c55.chunk.js"
+  },
+  {
+    "revision": "0166f0ace752684a4d74",
+    "url": "/Country-List/static/js/runtime-main.e3ae1870.js"
+  },
+  {
+    "revision": "3efb7b23d08f73cc93c79f24b60d004e",
+    "url": "/Country-List/static/media/flags.3efb7b23.png"
+  },
+  {
+    "revision": "daa6477004ad561abbdb5e6f770c2064",
+    "url": "/Country-List/static/media/happy_earth.daa64770.png"
+  },
+  {
+    "revision": "c33a86a48f4bcd2d323f1ae0b1dcbbf7",
+    "url": "/Country-List/static/media/happy_earth_ellipses.c33a86a4.png"
+  }
+]);
